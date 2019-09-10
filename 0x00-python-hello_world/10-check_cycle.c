6c975dccb0ff7b177680d81e61ee619928d5a89e
dalexach <dalexach@gmail.com>
2019-09-10 01:57:56 +0000

    Previous to 10th point

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,9 @@
+#include <lists.h>
+/**
+* check_cycle - Function that checls if a singly linked list has a cycle in it
+* @list: list to check
+* Return: 0 if there is no cyrcle, 1 if there is a cycle
+*/
+int check_cycle(listint_t *list)
+{
+}
