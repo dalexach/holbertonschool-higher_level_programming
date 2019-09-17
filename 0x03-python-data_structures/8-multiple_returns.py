2ef5412ac60f16c4f93f6de9f127a5a052f8643e
dalexach <dalexach@gmail.com>
2019-09-17 20:08:59 +0000

    Pycodestyle correction: delete a trailing whithespace

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@
 def multiple_returns(sentence):
     leng = len(sentence)
     if leng == 0:
-        first_ch = None 
+        first_ch = None
     else:
         first_ch = sentence[0]
     return (leng, first_ch)
