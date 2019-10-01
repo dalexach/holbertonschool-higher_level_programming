3723796c0a4f6b41e3dc7aa57597418439be7b07
dalexach <dalexach@gmail.com>
2019-10-01 20:58:43 +0000

    class Square that defines a square by: Private instance attribute: size:
        property def size(self): to retrieve it
        property setter def size(self, value): to set it
        Public instance method: def my_print(self): that prints in stdout the square with the character #:
    
        if size is equal to 0, print an empty line

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Create a class Square, includes setters and getters for size
+    """
+
+    def __init__(self, size=0):
+        """ Initialize Square class.
+        Arguments:
+        @size: Size of the square.
+        """
+        self.__check_size__(size)
+        self.__size = size
+
+    @property
+    def size(self):
+        """Set size as a property.
+        Return:
+        Value of size
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ Setter of size.
+        Arguments:
+        @value: value of size.
+        """
+        self.__check_size__(value)
+        self.__size = value
+
+    def area(self):
+        """ Calculate the area of a square.
+        Return:
+        Power of square size
+        """
+        return ((self.__size) ** 2)
+
+    def my_print(self):
+        """ Print a square with character # """
+        if self.size == 0:
+            print()
+        else:
+            for r in range(self.size):
+                for c in range(self.size):
+                    print("#",end="")
+                print()
+
+    def __check_size__(self, size):
+        """ 
+        Size error checking
+        Arguments:
+        @size: value of size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
