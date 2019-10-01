6a6b5a5fb7dbc2e0ab11b2e40c729ccd09c16488
dalexach <dalexach@gmail.com>
2019-10-01 19:18:17 +0000

    Class Square that defines a squeare by a private instance attribute size and instantiation with size(no type/value verification)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """ Create a class Square. """
+
+    def __init__(self, size):
+        """ Initialize Square class.
+        Arguments:
+        @size: Size of the square.
+        """
+        self.__size = size
