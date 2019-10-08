6b87245e13955a43513931471385c539b9b66302
dalexach <dalexach@gmail.com>
2019-10-08 04:38:06 +0000

    correction on init

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -16,8 +16,8 @@ class Rectangle:
         @width: width of the rectangle
         @height: height of the rectangle
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def __str__(self):
