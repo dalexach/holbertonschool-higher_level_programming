6c5ce998d395e83b005506688373edf4bb4d1c32
dalexach <dalexach@gmail.com>
2019-10-15 17:19:49 +0000

    Function that verify if an object is a exactly an instance of the specified class, if it's true return True, otherwise False

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Function that verify if an object is a exactly an instance of the specified
+class, if it's true return True, otherwise False
+"""
+def is_kind_of_class(obj, a_class):
+    """
+    Arguments:
+    @obj: object to verify
+    @a_class: class of the object to verify
+    Return:
+    True if the object is exactly an instance of the class, otherwise False
+    """
+    if isinstance(obj, a_class):
+        return (True)
+    else:
+        return (False)
