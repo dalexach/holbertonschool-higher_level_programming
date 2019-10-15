5ce6bca971ab4bc961a7496994ee1d31ca29de03
dalexach <dalexach@gmail.com>
2019-10-15 19:33:00 +0000

    Class Rectangle that inherits form BaseGeometry and PEP8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -26,6 +26,7 @@ class BaseGeometry():
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """ Class Rectangle """
     def __init__(self, width, height):
