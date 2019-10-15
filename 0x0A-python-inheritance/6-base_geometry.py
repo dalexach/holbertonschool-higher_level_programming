874815b53629e8c488739f149bdd10f6047cdda9
dalexach <dalexach@gmail.com>
2019-10-15 17:48:51 +0000

    Implement the public instance method area(self) that raise an Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+BaseGeometry module
+"""
+
+
+class BaseGeometry():
+    """ Class BaseGeometry """
+
+    def area(self):
+        """ Public instance method """
+        raise Exception("area() is not implemented")
