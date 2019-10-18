ffd3a76cd5478dea5644b0abf8940f6055d07ba1
dalexach <dalexach@gmail.com>
2019-10-18 23:11:03 +0000

    Create the class Square that inherits from Rectangle and overload the __str__ method

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module class Square that inherits from Rectangle
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square - class for a square, this class inherit form Rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Constructor for square
+        Arguments:
+        @size: size of the rectangle
+        @x: position in x
+        @y: position in y
+        @id: amount of instances created
+        """
+        super().__init__(size, size, x, y, id)
+
+    # magic methods
+    def __str__(self):
+        """
+        formats string representation of the Square
+        """
+        return ("[{}] ({}) {}/{} - {}".format(
+                self.__class__.__name__, self.id,
+                self.x, self.y, self.width))
