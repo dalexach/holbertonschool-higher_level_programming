8ba71a40e51f516fed94a3b5ad25985ddf884548
dalexach <dalexach@gmail.com>
2019-11-25 18:00:32 +0000

    showing the full description of the table, changing the way for create

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- Print the full description of the table from the database, without DESCRIBE or EXPLAIN
 -- table: first_table, database: Argument(hbtn_0c_0)
-SHOW COLUMNS FROM first_table;
+SHOW CREATE TABLE first_table;
