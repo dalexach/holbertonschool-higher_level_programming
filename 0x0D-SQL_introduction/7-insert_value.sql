8d3a95bf372264c35bf9930806e328bf4920cf74
dalexach <dalexach@gmail.com>
2019-11-25 19:27:01 +0000

    Inserting a new row in the tabble first_table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
---
---
-
+-- Insert a new row in a table of database
+-- new row: id=89, name=Holberton School; table: first_table, database: Argument(hbtn_0c_0)
+INSERT INTO first_table (id, name) VALUES (89,'Holberton School');
