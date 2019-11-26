5ead628849d7f2afd307e47f2141b03c286f7e7a
dalexach <dalexach@gmail.com>
2019-11-26 16:20:35 +0000

    list all the cities of California that can ve found in the database

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
--- 
--- 
-
+-- Listing all the cities of California founded in the database
+-- database: hbtn_0d_usa
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id;
