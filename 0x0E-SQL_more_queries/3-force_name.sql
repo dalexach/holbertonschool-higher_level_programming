809d416b10eb148a16fde805c5f761cecf4a4c33
dalexach <dalexach@gmail.com>
2019-11-26 15:00:55 +0000

    Creating a table with a description and not null in one value

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
--- 
--- 
-
+-- Creating a table
+-- table name: force_name; description table: id INT, name VARCHAR(256) can't be null
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
