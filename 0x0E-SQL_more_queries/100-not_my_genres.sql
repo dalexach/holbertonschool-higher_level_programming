1cee65a2447d5240d3a7f1641eec0eab710a845a
dalexach <dalexach@gmail.com>
2019-11-26 19:58:05 +0000

    Not my genre without repetitions

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,6 +1,6 @@
 -- List all genres of the show is different to Dexter 
 -- database: hbtn_0d_tvshows
-SELECT tv_genres.name 
+SELECT DISTINCT tv_genres.name 
 FROM tv_shows 
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
