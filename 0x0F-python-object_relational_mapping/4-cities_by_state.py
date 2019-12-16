bf03489781d5c7e186c8567582bb62f4d0bebef0
dalexach <dalexach@gmail.com>
2019-12-16 03:21:48 +0000

    Listing all cities from database using only one execute

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
 """
+Display all cities from hbtn_0e_4_usa
+Arguments:
+    username - username to connect the mySQL
+    mysql passwd - password to connect the mySQL
+    DB name - Name of the database
 """
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id ASC")
+    lists = cur.fetchall()
+    for row in lists:
+        print(row)
+    cur.close()
+    db.close()
