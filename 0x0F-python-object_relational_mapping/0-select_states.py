f60214208866efeb0151c96e4b650b526c7029aa
dalexach <dalexach@gmail.com>
2019-12-16 02:31:53 +0000

    Selecting states from a database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 """
+List all states from DB hbtn_0e_usa
+Arguments:
+    username - username to connect the mySQL
+    mysql passwd - password to connect the mySQL
+    DB name - Name of the database
 """
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+    lists = cur.fetchall()
+    for row in lists:
+        print(row)
+    cur.close()
+    db.close()
