8da6605454a150bdce7ca91665859477ff2af159
dalexach <dalexach@gmail.com>
2019-12-16 02:51:41 +0000

    Taken an argument with a state name searched, displays all values in the states table

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,3 +1,26 @@
 #!/usr/bin/python3
 """
+Display all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+Arguments:
+    username - username to connect the mySQL
+    mysql passwd - password to connect the mySQL
+    DB name - Name of the database
+    name - state name searched
 """
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}'\
+                ORDER BY states.id ASC".format(argv[4]))
+    lists = cur.fetchall()
+    for row in lists:
+        if row[1] == argv[4]:
+            print(row)
+    cur.close()
+    db.close()
