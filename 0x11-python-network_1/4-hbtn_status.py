4ddd2c70c51004b25ad6b065443c811e4ca744f7
dalexach <dalexach@gmail.com>
2020-01-13 17:46:46 +0000

    Package Requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+    Python script that fetches https://intranet.hbtn.io/status using requests
+"""
+
+import requests
+
+
+if __name__ == "__main__":
+    """
+        Using the package requests
+    """
+    response = requests.get("https://intranet.hbtn.io/status").text
+    print("Body response:")
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
