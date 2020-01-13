dfdb6abeb8f35279ef6ae05f3c49bcac5a67836d
dalexach <dalexach@gmail.com>
2020-01-13 17:59:48 +0000

    sends a POST request to the passed URL with the email as a parameter

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+    Python script that takes in a URL and an email address, sends a POST
+    request to the passed URL with the email as a parameter, and finally
+    displays the body of the response.
+    using the package requests
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """
+        Using the package Requests to send POST request
+    """
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    response = requests.post(url, value)
+    print(response.text)
+    
