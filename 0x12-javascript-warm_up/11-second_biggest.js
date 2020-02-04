f40d9b9c75c0274498b952364d1aa60d8412b461
dalexach <dalexach@gmail.com>
2020-02-04 21:42:56 +0000

    try withouth length

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,5 +3,5 @@
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  console.log(process.argv.splice(2, process.argv.length - 1).sort().reverse()[1]);
+  console.log(process.argv.splice(2).sort().reverse()[1]);
 }
