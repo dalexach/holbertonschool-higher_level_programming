daa1d69a2cd267735893521d81b2f8cfdcee7315
dalexach <dalexach@gmail.com>
2020-02-04 21:08:50 +0000

    First loop in JS

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const len = array.length;
+let i;
+
+for (i = 0; i < len; i++) {
+  console.log(array[i]);
+}
