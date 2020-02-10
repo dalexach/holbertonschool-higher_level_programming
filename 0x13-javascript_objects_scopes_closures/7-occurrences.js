af7ac78a9c96b6654056c6177f2ed662124c2ed4
dalexach <dalexach@gmail.com>
2020-02-10 20:17:04 +0000

    Finding the ocurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  let i;
+
+  for (i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      counter++;
+    }
+  }
+
+  return (counter);
+};
